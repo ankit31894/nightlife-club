3d67b863652654846765b1c5d4df079500cd339d
Ankit <ankit31894@gmail.com>
2016-09-24 19:25:36 +0530

    fixed redirect url

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     'googleAuth' : {
         'clientID'      : '1046770393727-mn32l83d7d2jtppbj74bna3bf8l9pila.apps.googleusercontent.com',
         'clientSecret'  : 'bUEN6jFNAwRbNM2kchzcBR5I',
-        'callbackURL'   : 'httpss://ankit31894-nc.herokuapp.com/login/google/callback'
+        'callbackURL'   : 'https://ankit31894-nc.herokuapp.com/login/google/callback'
     }
 
 };
